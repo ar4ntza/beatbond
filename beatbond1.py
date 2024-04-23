735b50c93ba73629fa4f9d892d52d1ff2e1af50f
Zammcrts <samcortes480@gmail.com>
2024-04-23 14:58:11 -0600

    arreglo mainpage

--- beatbond1.py
+++ beatbond1.py
@@ -13,14 +13,14 @@ mainpage.config(background='#FBEBC7')
 logo_image = Image.open('images/beatbond_logo.png')
 logo = ctk.CTkImage(light_image=logo_image, dark_image=logo_image, size=(500, 500))  # width x height
 logo_label = ctk.CTkLabel(mainpage, text="", image=logo, bg_color="#FFD700")  # Color de fondo amarillo para el logo
-logo_label.pack(pady=50)
+logo_label.pack(pady=2)
 
 
-slogan = ctk.CTkLabel(mainpage, text="Revolutionize your sound", font=("Helvetica", 80), bg_color="#FBEBC7")  # Color de fondo beige para el slogan
+slogan = ctk.CTkLabel(mainpage, text="Revolutionize your sound", font=("Helvetica", 80), bg_color="#FBEBC7", text_color="#5A2B71")  # Color de fondo beige para el slogan
 slogan.pack()
 
 # Botón de iniciar
-boton_iniciar = ctk.CTkButton(mainpage, text="⭐Empezar⭐", corner_radius=32, fg_color="#D03561", hover_color="#EA516D", border_color='#FFCC70', font=("Helvetica", 50))
+boton_iniciar = ctk.CTkButton(mainpage, text="⭐Empezar⭐", corner_radius=32, fg_color="#D03561", hover_color="#EA516D", border_color='#FFCC70', font=("Helvetica", 50),  bg_color="#FBEBC7")
 boton_iniciar.pack()
 
 mainpage.mainloop()
