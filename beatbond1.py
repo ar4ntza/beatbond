98d4c231ec0ca50104d0f2d6004a5362bc81c225
Zammcrts <samcortes480@gmail.com>
2024-04-23 13:54:59 -0600

    Co-authored-by: ar4ntza <ar4ntza@users.noreply.github.com>

--- beatbond1.py
+++ beatbond1.py
@@ -15,7 +15,7 @@ logo = ctk.CTkImage(light_image=Image.open('images/beatbond_logo.png'),
                     dark_image=Image.open('images/beatbond_logo.png'),
                     size=(500,500)) # width x height
 logo_label = ctk.CTkLabel(mainpage, text="", image=logo)
-logo_label.pack(pady=50, padx=20) 
+logo_label.pack(pady=50) 
 
 # boton de iniciar
 boton_iniciar = ctk.CTkButton(mainpage, text="⭐Empezar⭐", corner_radius=32, fg_color="#D03561", hover_color="#EA516D", border_color='#FFCC70', font=("Helvetica", 50))
