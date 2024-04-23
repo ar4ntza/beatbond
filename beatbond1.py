9b480bb1271656d067bb5472489dfc8b737f3ae6
Zammcrts <samcortes480@gmail.com>
2024-04-23 14:38:43 -0600

    Co-authored-by: ar4ntza <ar4ntza@users.noreply.github.com>

--- beatbond1.py
+++ beatbond1.py
@@ -1,25 +1,29 @@
-#libreria 
+# Librería
 import customtkinter as ctk
-from PIL import Image
+from PIL import Image, ImageTk
 
-# tema
-#mainpage
+# Tema
+# MainPage
 mainpage = ctk.CTk()
 mainpage.title("BeatBond")
 mainpage.geometry("700x700")
-
 mainpage.config(background='#FBEBC7')
 
-# logo
-logo = ctk.CTkImage(light_image=Image.open('images/beatbond_logo.png'), 
-                    dark_image=Image.open('images/beatbond_logo.png'),
-                    size=(500,500)) # width x height
-logo_label = ctk.CTkLabel(mainpage, text="", image=logo)
-logo_label.pack(pady=50) 
+# Logo
+logo_image = Image.open('images/beatbond_logo.png')
+logo = ctk.CTkImage(light_image=logo_image, dark_image=logo_image, size=(500, 500))  # width x height
+logo_label = ctk.CTkLabel(mainpage, text="", image=logo, bg_color="#FFD700")  # Color de fondo amarillo para el logo
+logo_label.pack(pady=50)
 
-# boton de iniciar
+
+slogan = ctk.CTkLabel(mainpage, text="Revolutionize your sound", font=("Helvetica", 80), bg_color="#FBEBC7")  # Color de fondo beige para el slogan
+slogan.pack()
+
+# Botón de iniciar
 boton_iniciar = ctk.CTkButton(mainpage, text="⭐Empezar⭐", corner_radius=32, fg_color="#D03561", hover_color="#EA516D", border_color='#FFCC70', font=("Helvetica", 50))
+boton_iniciar.pack()
 
-
-boton_iniciar.pack(pady=60)
 mainpage.mainloop()
+
+# Asegúrate de cerrar las imágenes después de que la ventana principal se cierre
+logo_image.close()
